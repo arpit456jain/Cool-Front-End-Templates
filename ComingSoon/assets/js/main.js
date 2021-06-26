0b22c445d179da84f3a3d104d59b7e9851a4738f
Arpit <111arpit1@gmail.com>
2021-06-26 22:19:45 +0530

    added coming soon template

---
+++ ComingSoon/assets/js/main.js
@@ -0,0 +1,15 @@
+
+!(function($) {
+  "use strict";
+
+  // Set the count down timer
+  if ($('.countdown').length) {
+    var count = $('.countdown').data('count');
+    var template = $('.countdown').html();
+    $('.countdown').countdown(count, function(event) {
+      $(this).html(event.strftime(template));
+    });
+  }
+
+
+})(jQuery);
