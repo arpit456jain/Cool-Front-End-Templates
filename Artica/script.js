0d06010c017108c8b98f780bb8c18dd8e76fad64
Sophiya Singh <sophiya.singh2002@gmail.com>
2021-07-11 22:56:44 +0530

    Added Project Artica

---
+++ Artica/script.js
@@ -0,0 +1,10 @@
+// function show(){
+//     document.getElementsByClassName("content").display="block";
+    
+// }
+var typed = new Typed('.type', {
+    strings: ['Vectors', 'Paintings', 'Photos', 'Animations'],
+    typeSpeed: 100,
+    backSpeed:70,
+    loop:true,
+  });
