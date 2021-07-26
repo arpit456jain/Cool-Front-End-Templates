8e7cc42cc6ee2db30ce1ea14c2281430a811ac8a
Jigar Sable <64949957+jigar-sable@users.noreply.github.com>
2021-07-26 12:03:06 +0530

    Added Preview Images (#106)
    * ui changes

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ fetch('style_links.json')
         let links = data;
 
         for (let i = 0; i < links.length; i++) {
+            console.log(links.length);
             let StyleBoxDiv = document.createElement('div');
             StyleBoxDiv.innerHTML = `<div class="stylebox">
             <div class="image">
-                 <img src="${links[i].preview}" alt="">
+                 <img src="https://raw.githubusercontent.com/arpit456jain/Cool-Front-End_Templates/master/${links[i].name}/preview.png" alt="">
              </div>
             <p>${links[i].name}</p>
             <a target="_blank" href="${links[i].link}">View</a>
