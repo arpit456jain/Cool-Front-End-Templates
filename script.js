d96c0375f7552dba0399bb35a42293bd06613d32
Jigar Sable <64949957+jigar-sable@users.noreply.github.com>
2021-07-27 15:22:43 +0530

    added back to top (#115)

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// scroll to top starts
+var scrolltop = $('#scroll-top');
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 80) {
+        scrolltop.addClass('active');
+    } else {
+        scrolltop.removeClass('active');
+    }
+});
+    
+scrolltop.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '200');
+});
+// scroll to top ends
+
+
 fetch('style_links.json')
     .then(response => response.json())
     .then(data => {
@@ -16,5 +34,4 @@ fetch('style_links.json')
             document.getElementById('styles').appendChild(StyleBoxDiv);
         };
 
-    });
-
+});
