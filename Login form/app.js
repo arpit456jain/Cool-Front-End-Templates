a9dea41446c09b629703b3abe910cd2d53e3abf7
Tanmay Trivedi <67366482+tany1014@users.noreply.github.com>
2021-08-02 22:41:43 +0530

    Added login form (#127)

---
+++ Login form/app.js
@@ -0,0 +1,15 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+var loader = document.getElementById('pre');
+window.addEventListener("load", function(){
+  loader.style.display="none"
+})
