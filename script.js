19c144c07515ea35dd863aa101e568c8cd6d083e
Kheya <kheyadas1919@gmail.com>
2022-03-02 20:32:33 +0530

    card UI updated

--- script.js
+++ script.js
@@ -1,58 +1,57 @@
 // Navbar Toggling starts
 
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 
-// Navbar Toggling end 
-
+// Navbar Toggling end
 
 // scroll to top starts
-var scrolltop = $('#scroll-top');
+var scrolltop = $("#scroll-top");
 
-$(window).scroll(function() {
-    if ($(window).scrollTop() > 80) {
-        scrolltop.addClass('active');
-    } else {
-        scrolltop.removeClass('active');
-    }
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 80) {
+    scrolltop.addClass("active");
+  } else {
+    scrolltop.removeClass("active");
+  }
 });
-    
-scrolltop.on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({scrollTop:0}, '200');
+
+scrolltop.on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "200");
 });
 // scroll to top ends
 
+fetch("style_links.json")
+  .then((response) => response.json())
+  .then((data) => {
+    let links = data;
 
-fetch('style_links.json')
-    .then(response => response.json())
-    .then(data => {
-        let links = data;
-
-        for (let i = 0; i < links.length; i++) {
-            console.log(links.length);
-            let StyleBoxDiv = document.createElement('div');
-            StyleBoxDiv.innerHTML = `<div class="stylebox">
+    for (let i = 0; i < links.length; i++) {
+      console.log(links.length);
+      console.log(links);
+      let StyleBoxDiv = document.createElement("div");
+      StyleBoxDiv.innerHTML = `<div class="stylebox">
             <div class="image">
                  <img src="https://raw.githubusercontent.com/arpit456jain/Cool-Front-End_Templates/master/${links[i].name}/preview.png" alt="">
              </div>
             <p>${links[i].name}</p>
-            
-            <div class="btn-group">
+      
+         <div class="btn-group">
             <a target="_blank" href="${links[i].link}">View</a>
-
-            </div>
+        </div>
+        
+           
 
             </div>`;
-            document.getElementById('styles').appendChild(StyleBoxDiv);
-        };
-
-});
+      document.getElementById("styles").appendChild(StyleBoxDiv);
+    }
+  });
 
 //<a target="_blank" href="https://github.com/arpit456jain/Cool-Front-End-Templates/tree/master/${links[i].name}" class="code">Code</a>
