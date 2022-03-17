6c45646e22d3ce451834bd8477cdc372a5be240c
Abhishek SIngh <71981463+Abh1shekSingh@users.noreply.github.com>
2022-03-17 22:41:05 +0530

    added searchbar in the website to filter out and search the projects (#454)

--- script.js
+++ script.js
@@ -30,29 +30,53 @@ scrolltop.on('click', function(e) {
 // scroll to top ends
 
 
-fetch('style_links.json')
-    .then(response => response.json())
-    .then(data => {
-        let links = data;
+let projects = [] ;
+const allProjectList = document.getElementById('styles');
+console.log(allProjectList)
+const searchBar = document.getElementById("searchBar");
+console.log(searchBar);
 
-        for (let i = 0; i < links.length; i++) {
-            console.log(links.length);
-            let StyleBoxDiv = document.createElement('div');
-            StyleBoxDiv.innerHTML = `<div class="stylebox">
-            <div class="image">
-                 <img src="https://raw.githubusercontent.com/arpit456jain/Cool-Front-End_Templates/master/${links[i].name}/preview.png" alt="">
-             </div>
-            <p>${links[i].name}</p>
-            
-            <div class="btn-group">
-            <a target="_blank" href="${links[i].link}">View</a>
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value;
+    const filteredProjects = projects.filter( projects => {
+        
+        return projects.name.toLowerCase().includes(searchString.toLowerCase());
+    });
 
-            </div>
+    console.log(filteredProjects)
+    displayProjects(filteredProjects);
+});
 
-            </div>`;
-            document.getElementById('styles').appendChild(StyleBoxDiv);
-        };
+const getProject = fetch('style_links.json')
+                .then(response => response.json())
+                .then(data => {
+                    projects = data;
+                    displayProjects(projects)
+                    console.log(projects)
 
 });
+
+ const displayProjects = (projects) => {
+
+    const htmlString = projects
+    .map((project) => {
+        return`<div class="stylebox">
+             <div class="image">
+                 <img src="https://raw.githubusercontent.com/arpit456jain/Cool-Front-End_Templates/master/${project.name}/preview.png" alt="">
+              </div>
+              <div class="card-data">
+                  <p class="card-heading">${project.name}</p>
+                  <a href=${project.link}><button class="btnn">View</button></a>
+              </div>
+       
+          </div>`;
+          
+    })
+    
+    allProjectList.innerHTML = htmlString;
+    
+}
+
+getProject();
 
 //<a target="_blank" href="https://github.com/arpit456jain/Cool-Front-End-Templates/tree/master/${links[i].name}" class="code">Code</a>
