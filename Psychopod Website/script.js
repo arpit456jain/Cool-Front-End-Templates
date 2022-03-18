d77e1ef657373bcf908d159a682c31c2012727f9
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2022-03-18 16:41:07 +0530

    PsychoPod Website Responsive Template Added

--- Psychopod Website/script.js
+++ Psychopod Website/script.js
@@ -1,3 +1,17 @@
-function func(){
-    window.alert("Your message has been sent successfully.");
-}
+const navMenu= document.getElementById('navMenu'),
+    sideNavbar= document.getElementById('sideNavbar'),
+    navClose= document.getElementById('navClose');
+
+    // showing menu
+    sideNavbar.addEventListener('click', ()=>{
+        navMenu.classList.toggle('show')
+    })
+
+    //hiding menu
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show')
+    })
+
+    function func(){
+        window.alert("Your message has been sent successfully.");
+    }
