51d712625d277b6b08b31ba775c5a5bdc3cba17a
TANYA-CHAN <tanyachanchalani@gmail.com>
2022-05-10 11:47:36 +0530

    Added Letter Animation Project to the file

---
+++ LetterAnimation/README.md
@@ -0,0 +1,4 @@
+# letter-animation
+A simple letter animation layout using HTML, CSS &amp; JS.
+<h2>Project Link</h2>
+<a href="https://tanya-chan.github.io/letter-animation/">Click Here!</a>
---
+++ LetterAnimation/script.js
@@ -0,0 +1,37 @@
+const spans = document.querySelectorAll('.word span');
+
+spans.forEach((span, idx) => {
+	span.addEventListener('click', (e) => {
+		e.target.classList.add('active');
+	});
+	span.addEventListener('animationend', (e) => {
+		e.target.classList.remove('active');
+	});
+	
+	// Initial animation
+	setTimeout(() => {
+		span.classList.add('active');
+	}, 750 * (idx+1))
+});
+
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
